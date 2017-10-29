eb7884da785918449e23e686a1493aa92d1a1ff2
Kyle Neil <kyle@johnneil.net>
2017-10-29 12:43:56 -0700

    started re-learning cpp!

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,8 +1,19 @@
-#include <header.h>
+#include "header.h"
 using namespace std;
 
-int main(int argc, char **argv) {
-	cout << "Hello world!" << endl;
+int func (int x, int y) {
+	return x / 2;
+}
 
+int main(int argc, char **argv) {
+	
+	int *heap = new int[4096]; //for kicks
+	assert(heap); //check for allocation error
+	
+	cout << func(6, 7) << endl;
+
+
+	
+	delete [] heap;
 	return 0;
 }
