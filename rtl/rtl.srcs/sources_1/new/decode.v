3d81514956a24512f0929a3dd7f4b3bd56bef09c
Kyle Neil <kyle@johnneil.net>
2018-05-26 06:04:22 +0000

    moved everything over to a fresh project, should still synthesize...?

---
+++ rtl/rtl.srcs/sources_1/new/decode.v
@@ -0,0 +1,14 @@
+/*
+    decode
+    instantiate the register file and any other signals that need to get passed through
+    
+*/
+
+module decode(input clk, input en, input [15:0] instr);
+    //make a regfile, send out decoded instructions to EX, MEM, and WB units
+    // :D
+    
+    
+    
+    
+endmodule
---
+++ rtl/rtl.srcs/sources_1/new/fetch.v
@@ -0,0 +1,24 @@
+/*
+    Instruction fetch unit
+    instantiate PC, fetch instruction
+*/
+
+
+module fetch(input clk, input en, input rst, output [15:0] instr);
+
+    reg pc_load_count = 0;
+    
+    reg mem_read_write = 0;
+    reg [15:0] mem_in = 0;
+    
+    reg [15:0] pc_load = 0;
+    wire [15:0] pc_out;
+    
+    //module memory(input clk, input read_write, input [15:0] addr, input [15:0] in, output reg [15:0] out);
+    //module pc(input clk, input en, input rst, input load_count, input [15:0] load, output reg [15:0] pc);
+    
+    pc p(.clk(clk), .en(en), .rst(rst), .load_count(pc_load_count), .load(pc_load), .pc(pc_out));
+    memory m(.clk(clk), .read_write(mem_read_write), .addr(pc_out), .in(mem_in), .out(instr));
+    
+    
+endmodule
