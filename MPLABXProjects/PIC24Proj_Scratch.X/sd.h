d599e44ac9c3991491866e9f7da28cd588176899
Kyle Neil <kyle@johnneil.net>
2018-06-18 12:34:58 -0700

    more sd card stuff

--- MPLABXProjects/PIC24Proj_Scratch.X/sd.h
+++ MPLABXProjects/PIC24Proj_Scratch.X/sd.h
@@ -4,7 +4,7 @@ typedef struct sd_command { //basic SD command structure
     uint8_t crc; //crc, usually not checked unless a CMD0 is issued
 } sd_command;
 
-typedef struct sd_resp { //40-bit response for some commands, like CMD58
+typedef struct sd_resp { //40-bit response for some commands
     uint8_t resp[5];
 } sd_resp;
 
@@ -17,13 +17,12 @@ sd_command CMD55 = {.command = 0b01110111, .args[0] = 0, 0, 0, 0, .crc = 0};
 sd_command ACMD41 = {.command = 0b01101001, .args[0] = 0x40, 0, 0, 0, .crc = 0};
 sd_command CMD58 = {.command = 0b01111010, .args[0] = 0, 0, 0, 0, .crc = 0};
 
-sd_command CMD17 = {.command = 0b01010001, .args[0] = 0, 0, 0, 0, .crc = 0};
+sd_command CMD17 = {.command = 0b01010001, .args[0] = 0, 0, 0, 0, .crc = 0}; //read a single block
 
-typedef struct sd_block {
+typedef struct sd_block { //block size is almost universally 512 bytes
     uint8_t data[512];
 } sd_block;
-//could we use vargs for this?
-//Note: find out how the heck to calculate a CRC!
+
 uint8_t sd_writeCommand(sd_command *c) {
     SS_SetLow();
     //set upper two bits of com to 0x40 later, or check for this
@@ -61,21 +60,23 @@ sd_resp sd_writeCommandLong(sd_command *c) {
     while (result >= 0x80) {
         result = SPI2_Exchange8bit(0xFF);
     }
+    
     s.resp[0] = result;
-    s.resp[1] = SPI2_Exchange8bit(0xFF);
-    s.resp[2] = SPI2_Exchange8bit(0xFF);
-    s.resp[3] = SPI2_Exchange8bit(0xFF);
-    s.resp[4] = SPI2_Exchange8bit(0xFF);
+    for (int i = 1; i < 5; i++) {
+        s.resp[i] = SPI2_Exchange8bit(0xFF);
+    }
     
     SS_SetHigh();
     return s;
 }
 
+//read a block from the SD card
+//this is hard-coded because there are only two commands to do this
 void sd_readBlock(uint64_t block_addr, sd_block *rd_block) {
     SS_SetLow();
     SPI2_Exchange8bit(CMD17.command);
-    for (int i = 0; i < 4; i++) {
-        SPI2_Exchange8bit(0x00); //come back to this bit-twiddling crap
+    for (int i = 4; i > 0; i--) {
+        SPI2_Exchange8bit((block_addr >> (i * 8)) & 0xFF);
     }
     SPI2_Exchange8bit(CMD17.crc);
     
@@ -96,7 +97,8 @@ void sd_readBlock(uint64_t block_addr, sd_block *rd_block) {
     SS_SetHigh();
 }
 
-//note: a recent SD card is assumed.  Old cards will not work
+//note: a recent SD card is assumed.  Old cards will not work.
+//add error checking here!!
 uint8_t sd_init() {
     SPI2CON1bits.PPRE = 0b00; //set clock speed to ~125kHz
     for (int i = 0; i < 10; i++) {
@@ -112,8 +114,8 @@ uint8_t sd_init() {
     printf("CMD55, ACMD41\n");
     uint8_t result = 0xFF;
     do {
-        sd_writeCommand(&ACMD41);
-        result = sd_writeCommand(&CMD55);
+        sd_writeCommand(&CMD55);
+        result = sd_writeCommand(&ACMD41);
     } while (result != 0);
     
     r = sd_writeCommandLong(&CMD58);
