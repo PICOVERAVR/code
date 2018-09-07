c91aa6e868235f95e0350fc339f225838c8aff6a
Kyle Neil <kyle@johnneil.net>
2018-09-06 19:47:32 -0700

    misc x64 perf stuff and makefile modifications

--- c/glib/main.c
+++ c/glib/main.c
@@ -7,9 +7,8 @@ int main(int argc, char **argv) {
 	gpointer key = (gpointer) 4;
 	gpointer val = (gpointer) 65;
 	
-	g_hash_table_insert(ht, key, val);
+	g_hash_table_insert(ht, key, val);	
 	
-
 	gpointer x = g_hash_table_lookup(ht, key);
 	printf("key of %d, val of %d, got %d\n", (int) key, (int) val, (int) x);
 	
