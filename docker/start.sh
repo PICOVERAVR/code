ef922e994411f7233963f601d0dade2758a35c9d
Kyle Neil <kyle@johnneil.net>
2018-10-09 20:13:47 -0700

    docker containers now read from specialized docker bashrc

--- docker/start.sh
+++ docker/start.sh
@@ -1,7 +1,7 @@
 docker run -it --rm \
  -v /Users/kyleneil/code:/root/ \
  -v /Users/kyleneil:/mnt \
- -v /Users/kyleneil/.bashrc:/root/.bashrc \
+ -v /Users/kyleneil/code/main/docker/docker_bashrc:/root/.bashrc \
  --cap-add=SYS_PTRACE \
  --name base$1 base bash
 
