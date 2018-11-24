09c4974fee71abc1aface9825161f2ee3ce41047
Kyle Neil <kyle@johnneil.net>
2018-11-24 22:05:29 +0000

    more runtime stuff

--- cpp/header.h
+++ cpp/header.h
@@ -4,16 +4,16 @@
 
 class base {
 	public:
-	void base_f() { std::cout << "Base function!" << std::endl; }
+	virtual void f() { std::cout << "Base function!" << std::endl; }
 };
 
 class d1: public base {
 	public:
-	void d1_f() { std::cout << "Derived 1 function!" << std::endl; }
+	void f() { std::cout << "Derived 1 function!" << std::endl; }
 };
 
 class d2: public base {
 	public:
-	void d2_f() { std::cout << "Derived 2 function!" << std::endl; }
+	void f() { std::cout << "Derived 2 function!" << std::endl; }
 };
 
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -3,8 +3,15 @@ using namespace std;
 
 int main(int argc, char **argv) {
 	//int *heap = new int[4096]; //for kicks
+	d1 d;
+	d2 d_2;
+	base *b, *b2;
 	
+	b = &d;
+	b2 = &d_2;
 	
+	b->f();
+	b2->f();
 	
 	//delete [] heap;
 	return 0;
