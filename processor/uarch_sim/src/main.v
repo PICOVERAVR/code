eca3f8581c0d3034c343ee03c1f1527c702076b9
Kyle Neil <kyle@johnneil.net>
2018-11-29 15:15:41 -0800

    now working on uarch-sim

---
+++ processor/uarch_sim/src/main.v
@@ -0,0 +1,45 @@
+/*
+	using this as an initial testbench
+*/
+
+`timescale 1ns / 1ns
+
+module main();
+	
+	reg clk = 0;
+	reg en = 0;
+
+	reg wr = 0;
+	reg [10:0] addr = 0;
+	reg [31:0] data_i = 0;
+
+	wire [31:0] data_o;
+	
+	 mem_unit m(.clk_i(clk), .en_i(en), .wr_i(wr), .addr_i(addr), .data_i(data_i), .data_o(data_o));
+
+	always #(1) begin
+		clk <= ~clk;	
+	end
+	
+	initial begin
+		$display("Running mem_unit testbench...");
+		$dumpfile("proc_dump.vcd");
+		$dumpvars(0, main);
+		
+		#1;
+		en <= 1'b1;
+		addr <= 11'h3;
+		data_i <= 32'hFF;
+		wr <= 1'b1;
+		#4;
+		wr <= 1'b0;
+		addr <= 11'h2;
+		#1;
+		en <= 1'b0;
+
+		#16;
+		
+		$display("Finished.");
+		$finish;
+	end
+endmodule
---
+++ processor/uarch_sim/src/mem.v
@@ -0,0 +1,37 @@
+/*
+	fetches instructions from memory.
+	initially just makes memory acceses controllable and synchronous.
+	
+	delay of .5 cycles when writing / reading
+
+	instruction will be updated if module is enabled.
+	otherwise, instruction will go hi-z
+*/
+
+`timescale 1ns / 1ns
+
+module mem_unit (
+	input clk_i,
+	input en_i,
+	input wr_i,
+
+	input [10:0] addr_i,
+
+	input [31:0] data_i,
+	output reg [31:0] data_o
+);
+	parameter ADDR_WIDTH = 11; // 2^11 = 2048 bytes
+	reg [31:0] mem_file [ADDR_WIDTH-1:0];
+
+	always @(posedge clk_i or negedge clk_i) begin
+		if (en_i) begin
+			if (wr_i) begin
+				mem_file[addr_i] <= data_i;
+			end
+			data_o <= mem_file[addr_i];
+		end else begin
+			data_o <= {32{1'hZ}};
+		end
+	end
+	
+endmodule
