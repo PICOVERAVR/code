2d56608eba8cdf786790b1e7bf67d2e6e060fc84
Kyle Neil <kyle@johnneil.net>
2018-12-01 23:42:36 -0800

    jesus C++ is different than C

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,11 +1,28 @@
-#include "header.h"
+#include <iostream>
 using namespace std;
 
+class base {
+    public:
+    virtual void f() { std::cout << "Base function!" << std::endl; }
+};
+
+class d1: public base {
+    public:
+    void f() { std::cout << "Derived 1 function!" << std::endl; }
+};
+
+class d2: public base {
+    public:
+    void f() { std::cout << "Derived 2 function!" << std::endl; }
+};
+
 int main(int argc, char **argv) {
-	//int *heap = new int[4096]; //for kicks
 	
+	int c = 4;
+	auto sum = [c] (auto a, int b) -> double { // jesus
+		return a * b + c;
+	};
 	
+	cout << sum(4, 3) << endl;
 	
-	//delete [] heap;
-	return 0;
 }
