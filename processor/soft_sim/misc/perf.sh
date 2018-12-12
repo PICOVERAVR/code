10ad904dee9ada94e92657867b4082210dc5a812
Kyle Neil <kyle@johnneil.net>
2018-12-11 20:01:28 -0800

    updated perf.sh to check for pv program

--- processor/soft_sim/misc/perf.sh
+++ processor/soft_sim/misc/perf.sh
@@ -1,3 +1,8 @@
 SRC=${1:-"../test/programs/jmp_add/jmp_add.bin"}
+if [ -e $(which pv) ]; then
+	echo "pv found."
+else
+	echo "pv not found."
+fi
 echo "Running sim with program $SRC..."
 ../src/fives -b $SRC | pv --line-mode --rate -N Instructions\ per\ second > /dev/null
