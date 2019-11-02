a5037fa99426f9fb2d74fb0247366bb3b79791d7
Kyle Neil <kyle@johnneil.net>
2019-11-02 13:39:57 -0700

    added a thing or two to the makefile

--- 68k/src/c/main.c
+++ 68k/src/c/main.c
@@ -1,6 +1,13 @@
 int x = 0xF00F;
 int y;
 
+void delay(int value) { // value in ms
+	const int delaycycles = (value * 1000) / 22;
+	volatile int current = 0;
+	while (current++ < delaycycles);
+}
+
 int main() {
+	delay(100);
 	return 0;
 }
