74f6b7f265b5b4eda308134fefcbf25f3d73d473
Kyle Neil <kyle@johnneil.net>
2020-01-06 08:03:07 +0000

    added multithreading support using C++ only!

--- math/integrate2.cpp
+++ math/integrate2.cpp
@@ -1,18 +1,8 @@
-// compile with clang++ main.cpp -Ofast -ffast-math -avx2 -o main.out
+// clang: clang++ main.cpp -Ofast -ffast-math -avx2 -o main.out
+// gcc: g++ main.cpp -Ofast -ffast-math -avx2 -pthread -o main.out
 #include <iostream>
 #include <limits>
-
-struct ends {
-	double xmin;
-	double xmax;
-	
-	double ymin;
-	double ymax;
-	
-	int xnum;
-	int ynum;
-
-};
+#include <future>
 
 // function to approximate
 double func(double x, double y) {
@@ -21,16 +11,16 @@ double func(double x, double y) {
 
 // basic method of approximation, using double riemann sums.
 // there are more advanced algorithms out there that get better results, are probably faster too
-double i2add(double (*f)(double, double), ends p) {
+double i2add(double xmin, double xmax, double ymin, double ymax, int xnum, int ynum) {
 	
-	const double dx = (p.xmax - p.xmin) / p.xnum;
-	const double dy = (p.ymax - p.ymin) / p.ynum;
+	const double dx = (xmax - xmin) / xnum;
+	const double dy = (ymax - ymin) / ynum;
 	
 	double sum = 0.0;
 	
-	for (double x = p.xmin; x < p.xmax; x += dx) {
-		for (double y = p.ymin; y < p.ymax; y += dy) {
-			sum += f(x + dx/2, y + dy/2);
+	for (double x = xmin; x < xmax; x += dx) {
+		for (double y = ymin; y < ymax; y += dy) {
+			sum += func(x + dx/2, y + dy/2);
 		}
 	}
 
@@ -45,15 +35,30 @@ int main(int argc, char **argv) {
 	const double ymin = 0.0;
 	const double ymax = 1.0;
 
-	const int size = 2<<15;
+	const int size = 2<<17;
 
 	double sum = 0.0;
 	
-	std::cout << "Integrating a function from " << xmin << " to " << xmax << " (x) and from " << ymin << " to " << ymax << " (y), " << (long)size << " samples per dimension" << std::endl;
-
-	const ends temp = { xmin, xmax, ymin, ymax, size, size };
-	sum = i2add(func, temp);
+	const int num_threads = std::thread::hardware_concurrency();
+	
+	const double xstep = (xmax - xmin) / num_threads;
+	
+	std::cout << "Integrating a function from " << xmin << " to " << xmax << " (x) and from " << ymin << " to " << ymax << " (y), " << (long)size << " samples per dimension." << std::endl;
+	std::cout << "Using " << num_threads << " threads." << std::endl;
+	
+	std::future<double> threadarr[num_threads];
+	for (int i = 0; i < num_threads; i++) {
+		threadarr[i] = std::async(i2add, xmin + xstep * i, xmin + xstep * (i + 1), ymin, ymax, size / num_threads, size);
+	}
+	
+	for (int i = 0; i < num_threads; i++) {
+		sum += threadarr[i].get();
+	}
+	
+	//double sumser = i2add(xmin, xmax, ymin, ymax, size, size);
 	
 	std::cout.precision(std::numeric_limits<double>::max_digits10); // print full numeric precision to see errors
-	std::cout << "Answer: " << sum << std::endl;
+	std::cout << "Parallel answer: " << sum << std::endl;
+	
+	//std::cout << "Serial answer: " << sumser << std::endl;
 }
